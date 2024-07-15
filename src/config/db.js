b27f5c7083a1c6a20762468aadf8b503b4aa7a2e
BRILLIO\Ayan.Dhiman <ayan.dhiman@signanthealth.com>
2024-07-15 12:11:37 +0530

    DB SSL Changes

--- src/config/db.js
+++ src/config/db.js
@@ -8,6 +8,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('db_server_kyze','db_server_kyze_user','8MJ7mSKCsPDbhgph2gblm4z462HDq0uX',{
     host: 'dpg-cq7mt4jv2p9s73c6u0og-a.oregon-postgres.render.com',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
 });
 
 module.exports = sequelize;
